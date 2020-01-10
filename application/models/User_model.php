cdb492fdc547418f25e32a05ec1065e6639cf446
Radhakishan Jangid <jangirr48@gmail.com>
2020-01-10 07:12:36 +0530

    Add files via upload

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,15 +2,15 @@
 
 defined('BASEPATH') or exit ("No direct script access allowed");
 
-class User_model extends CI_Model() {
+class User_model extends CI_Model {
 	private $table = 'users';
 
 	function get_all_user() {
-		$query = $this->db->select("*")->get($table);
+		$query = $this->db->select("*")->get($this->table);
 		return $query->result();
 	}
 
 	function getUniqueUserById($id) {
-		return $this->db->get_where($table, array('id' => $id));
+		return $this->db->get_where($this->table, array('id' => $id))->result_array();
 	}
 }
